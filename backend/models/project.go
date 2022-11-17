77074c893a7772e685113f6177bd13555a368c9d
lukakralik <luka.kralik@gmail.com>
2022-11-17 23:58:36 +0100

    DB introduction

---
+++ backend/models/project.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	_ "encoding/json"
+	_ "gorm.io/gorm"
+	_ "github.com/golang-module/carbon/v2"
+)
+
+type Project struct {
+	ID		int				`json:"id"`	
+	Name	string			`json:"name"`
+	Author	string			`json:"author"`
+	Rating	float32			`json:"rating"`
+	Time	carbon.Carbon	`json:"time"`
+}
